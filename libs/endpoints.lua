0daef83e02f255a3b99dc8c9f07f6d3e50c23a2b
Benbebop <benbebop@hotmail.com>
2023-02-03 03:37:32 -0800

    Create endpoints.lua

---
+++ libs/endpoints.lua
@@ -0,0 +1,8 @@
+return {
+	GLOBAL_COMMANDS = "/commands",
+	GLOBAL_COMMAND = "/commands/%s",
+	GUILD_COMMANDS = "/guilds/%s/commands",
+	GUILD_COMMAND = "/guilds/%s/commands/%s",
+	GUILD_COMMANDS_PERMISSIONS = "/guilds/%s/commands/permissions",
+	GUILD_COMMAND_PERMISSIONS = "/guilds/%s/commands/%s/permissions",
+}
