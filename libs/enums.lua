1b6af20ffc763c92609ed2367c7081131dec33cd
Benbebop <benbebop@hotmail.com>
2023-02-03 03:05:21 -0800

    Create enums.lua

---
+++ libs/enums.lua
@@ -0,0 +1,25 @@
+-- THIS IS DISCORDIA COMMAND BITCH, WE TAB IN THIS MUTHAFUCKA BETTA TAKE YO SPACE ASS BACK TO DISCORDIA INTERACTIONS
+
+local enums = {}
+
+enums.applicationCommandType = {
+	chatInput = 1,
+	user = 2,
+	message = 3
+}
+
+enums.applicationCommandOptionType = {
+	subCommand = 1,
+	subCommandGroup = 2,
+	string = 3,
+	integer = 4,
+	boolean = 5,
+	user = 6,
+	channel = 7,
+	role = 8,
+	mentionable = 9,
+	number = 10,
+	attachment = 11
+}
+
+return enums
