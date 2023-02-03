6020bed475455452cfcd3bb31c163efd46effc0e
Benbebop <benbebop@hotmail.com>
2023-02-03 06:24:11 -0800

    Fix wording

--- libs/endpoints.lua
+++ libs/endpoints.lua
@@ -3,6 +3,6 @@ return {
 	GLOBAL_COMMAND = "/commands/%s",
 	GUILD_COMMANDS = "/guilds/%s/commands",
 	GUILD_COMMAND = "/guilds/%s/commands/%s",
-	GUILD_COMMANDS_PERMISSIONS = "/guilds/%s/commands/permissions",
+	GLOBAL_COMMANDS_PERMISSIONS = "/guilds/%s/commands/permissions",
 	GUILD_COMMAND_PERMISSIONS = "/guilds/%s/commands/%s/permissions",
 }
