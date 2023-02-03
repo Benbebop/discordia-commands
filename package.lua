dd4311960ae8b2e42ed8cc4876e7eeb665345444
Benbebop <benbebop@hotmail.com>
2023-02-03 03:51:41 -0800

    Create package.lua
    I'm not actually planning on putting this on lit, but i'll include this anyway.

---
+++ package.lua
@@ -0,0 +1,12 @@
+-- not planning on putting this on lit, including this just in case tho
+
+return {
+	name = 'Benbebop/discordia-commands',
+	version = '1.0',
+	homepage = 'https://github.com/Benbebop/discordia-commands',
+	dependencies = {
+	},
+	tags = {'discord', 'api'},
+	author = 'Benbebop',
+	files = {'**.lua'},
+}
