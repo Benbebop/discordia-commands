0e555661a92b19fdf695f45f7d753e8043600333
Benbebop <benbebop@hotmail.com>
2023-02-04 22:55:10 -0800

    put stuff in client functions

--- libs/client/Client.lua
+++ libs/client/Client.lua
@@ -4,6 +4,8 @@
 ]=]
 
 local discordia = require("discordia")
+local Resolver = require("discordia/client/Client/Resolver")
+local enum = require("enums")
 
 local Command = require("containers/Command")
 
@@ -17,7 +19,7 @@ local Client = discordia.class.classes.Client
 @d Create or overwrite (if it already exists) a slash command. Providing a guild will make it a guild command, elsewise it will be a global command.
 ]=]
 function Client:newSlashCommand( name, guild )
-	
+	local c = Command( {_new = true, _type = enum.applicationCommandType.chatInput, _id = 0, _guild = guild and Resolver.guild(guild).id}, self, self )
 end
 
 --[=[
@@ -28,7 +30,7 @@ end
 @d Create or overwrite (if it already exists) a user command. Providing a guild will make it a guild command, elsewise it will be a global command.
 ]=]
 function Client:newUserCommand( name, guild )
-	
+	local c = Command( {_new = true, _type = enum.applicationCommandType.user, _id = 0, _guild = guild and Resolver.guild(guild).id}, self, self )
 end
 
 --[=[
@@ -39,7 +41,7 @@ end
 @d Create or overwrite (if it already exists) a message command. Providing a guild will make it a guild command, elsewise it will be a global command.
 ]=]
 function Client:newMessageCommand( name, guild )
-	
+	local c = Command( {_new = true, _type = enum.applicationCommandType.message, _id = 0, _guild = guild and Resolver.guild(guild).id}, self, self )
 end
 
 --[=[
@@ -49,26 +51,22 @@ end
 @d Get a global command.
 ]=]
 function Client:getGlobalCommand( id )
-	
+	local c = self._appCommands:find(function(c) return c.id == id end)
+	if not c then
+		c = Command( self._api:getGlobalApplicationCommand(id), self, self )
+	end
+	return c
 end
 
 --[=[
-@m getGuildCommand
-@p guild Guild-ID-Resolvable
+@m getCommand
 @p id Command-ID-Resolvable
 @r Command
-@d Get a guild command.
+@d Get a global command.
 ]=]
-function Client:getGuildCommand( guild, id )
-	
-end
-
---[=[
-@m cacheCommands
-@d Get all commands and cache them.
-]=]
-function Client:cacheCommands()
-	
+function Client:deleteGlobalCommand( id )
+	local c = self._appCommands:find(function(c) return c.id == id end)
+	c:delete()
 end
 
 --[=[ 
@@ -78,25 +76,22 @@ function Client.__getters:applicationCommands()
 	return self._appCommands
 end
 
---[=[ 
-@p slashCommands FilteredIterable All global slash commands currently cached to the client.
+--[=[
+@m cacheCommands
+@d Get all commands and cache them. This will overwrite any changes made on the same event loop.
 ]=]
-function Client.__getters:slashCommands()
-	return self._appCommands
+function Client:cacheCommands()
+	
 end
 
---[=[ 
-@p userCommands FilteredIterable All global slash commands currently cached to the client.
-]=]
-function Client.__getters:userCommands()
-	return self._appCommands
-end
+local oldClientInit = Client.__init
 
---[=[ 
-@p messageCommands FilteredIterable All global slash commands currently cached to the client.
-]=]
-function Client.__getters:messageCommands()
-	return self._appCommands
+function Client:__init()
+	self:on("ready", function()
+		self:cacheCommands()
+	end)
+	
+	return oldClientInit(self)
 end
 
 return Client
