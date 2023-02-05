087356cce13b8e9c0c9d0564ceb992b08546cc90
Benbebop <benbebop@hotmail.com>
2023-02-05 12:15:54 -0800

    store commands to cache

--- libs/client/Client.lua
+++ libs/client/Client.lua
@@ -5,6 +5,7 @@
 
 local discordia = require("discordia")
 local Resolver = require("discordia/client/Client/Resolver")
+local Cache = discordia.class.classes.Cache
 local enum = require("enums")
 
 local Command = require("containers/Command")
@@ -19,7 +20,9 @@ local Client = discordia.class.classes.Client
 @d Create or overwrite (if it already exists) a slash command. Providing a guild will make it a guild command, elsewise it will be a global command.
 ]=]
 function Client:newSlashCommand( name, guild )
-	local c = Command( {_new = true, _type = enum.applicationCommandType.chatInput, _id = 0, _guild = guild and Resolver.guild(guild).id, _name = name}, self, self )
+	local c = Command( {new = true, type = enum.applicationCommandType.chatInput, id = 0, guild = guild and Resolver.guild(guild).id, name = name}, self, self )
+	table.insert(self._commandTable, c)
+	return c
 end
 
 --[=[
@@ -30,7 +33,9 @@ end
 @d Create or overwrite (if it already exists) a user command. Providing a guild will make it a guild command, elsewise it will be a global command.
 ]=]
 function Client:newUserCommand( name, guild )
-	local c = Command( {_new = true, _type = enum.applicationCommandType.user, _id = 0, _guild = guild and Resolver.guild(guild).id, _name = name}, self, self )
+	local c = Command( {new = true, type = enum.applicationCommandType.user, id = 0, guild = guild and Resolver.guild(guild).id, name = name}, self, self )
+	table.insert(self._commandTable, c)
+	return c
 end
 
 --[=[
@@ -41,7 +46,9 @@ end
 @d Create or overwrite (if it already exists) a message command. Providing a guild will make it a guild command, elsewise it will be a global command.
 ]=]
 function Client:newMessageCommand( name, guild )
-	local c = Command( {_new = true, _type = enum.applicationCommandType.message, _id = 0, _guild = guild and Resolver.guild(guild).id, _name = name}, self, self )
+	local c = Command( {new = true, type = enum.applicationCommandType.message, id = 0, _guild = guild and Resolver.guild(guild).id, name = name}, self, self )
+	table.insert(self._commandTable, c)
+	return c
 end
 
 --[=[
@@ -51,9 +58,10 @@ end
 @d Get a global command.
 ]=]
 function Client:getGlobalCommand( id )
-	local c = self._appCommands:find(function(c) return c.id == id end)
+	local c = self._commandCache:find(function(c) return c.id == id end)
 	if not c then
 		c = Command( self._api:getGlobalApplicationCommand(id), self, self )
+		table.insert(self._commandTable, c)
 	end
 	return c
 end
@@ -65,7 +73,7 @@ end
 @d Get a global command.
 ]=]
 function Client:deleteGlobalCommand( id )
-	local c = self._appCommands:find(function(c) return c.id == id end)
+	local c = self._commandCache:find(function(c) return c.id == id end)
 	c:delete()
 end
 
@@ -73,7 +81,7 @@ end
 @p applicationCommands TableIterable All global commands currently cached to the client. Note: This is not updated by gateway events, you can request an update by using `Client:cacheCommands()`.
 ]=]
 function Client.__getters:applicationCommands()
-	return self._appCommands
+	return self._commandCache
 end
 
 --[=[
@@ -81,12 +89,19 @@ end
 @d Get all commands and cache them. This will overwrite any changes made on the same event loop.
 ]=]
 function Client:cacheCommands()
-	
+	local commands = self._api:getGlobalApplicationCommands()
+	for _,command in ipairs(commands) do
+		local c = Command( command, self, self )
+		table.insert(self._commandTable, c)
+	end
 end
 
 local oldClientInit = Client.__init
 
 function Client:__init()
+	self._commandTable = {}
+	self._commandCache = Cache( self._commandTable )
+	
 	self:on("ready", function()
 		self:cacheCommands()
 	end)
