7c0192b47c40539c62771566c585f6fbd4c55a76
Benbebop <benbebop@hotmail.com>
2023-02-07 13:46:23 -0800

    documentation

--- libs/client/Client.lua
+++ libs/client/Client.lua
@@ -1,8 +1,3 @@
---[=[
-@c Client
-@d All additions made to the Client class.
-]=]
-
 local discordia = require("discordia")
 local Resolver = require("client/Resolver")
 local Cache = discordia.class.classes.Cache
@@ -13,60 +8,20 @@ local Command = require("containers/Command")
 
 local Client = discordia.class.classes.Client
 
---[=[
-@m newSlashCommand
-@p name string
-@op guild Guild-ID-Resolvable
-@r Command
-@d Create or overwrite (if it already exists) a slash command. Providing a guild will make it a guild command, elsewise it will be a global command.
-]=]
 function Client:newSlashCommand( name, guild ) return shared.newCommand( guild and self:getGuild(Resolver.guildId(guild)) or self, self, "chatInput", name, guild ) end
 
---[=[
-@m newUserCommand
-@p name string
-@op guild Guild-ID-Resolvable
-@r Command
-@d Create or overwrite (if it already exists) a user command. Providing a guild will make it a guild command, elsewise it will be a global command.
-]=]
 function Client:newUserCommand( name, guild ) return shared.newCommand( guild and self:getGuild(Resolver.guildId(guild)) or self, self, "user", name, guild ) end
 
---[=[
-@m newMessageCommand
-@p name string
-@op guild Guild-ID-Resolvable
-@r Command
-@d Create or overwrite (if it already exists) a message command. Providing a guild will make it a guild command, elsewise it will be a global command.
-]=]
 function Client:newMessageCommand( name, guild ) return shared.newCommand( guild and self:getGuild(Resolver.guildId(guild)) or self, self, "message", name, guild ) end
 
---[=[
-@m getCommand
-@p id Command-Id-Resolvable
-@r Command
-@d Get a global command.
-]=]
 function Client:getGlobalCommand( id ) return shared.getCommand( self, Resolver.commandId( id ) ) end
 
---[=[
-@m getCommand
-@p id Command-ID-Resolvable
-@r Command
-@d Get a global command.
-]=]
 function Client:deleteGlobalCommand( id ) shared.deleteCommand( self, Resolver.commandId( id ) ) end
 
---[=[ 
-@p applicationCommands Cache All global commands currently cached to the client. This also contains guild commands if the client has not been run yet. Note: This is not updated by gateway events, you can request an update by using `Client:cacheCommands()`.
-]=]
 function Client.__getters:applicationCommands()
 	return self._commandCache
 end
 
---[=[
-@m cacheCommands
-@d Get all global commands and cache them.
-]=]
 function Client:cacheCommands() shared.cacheCommands(self) end
 
 local oldClientInit = Client.__init
@@ -91,6 +46,10 @@ function Client:__init( ... )
 			end
 		end
 		for _,v in ipairs(toRemove) do table.remove(self._commandTable, v) end
+	end)
+	
+	self:on("interactionCreate", function( ... )
+		p( ... )
 	end)
 	
 	return initResults
